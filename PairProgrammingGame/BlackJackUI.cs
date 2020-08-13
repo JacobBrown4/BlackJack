8fb558f7ab9f5d61e1e153cb3639ad4156c392d1
JacobBrown4 <68622976+JacobBrown4@users.noreply.github.com>
2020-08-12 23:04:44 -0400

    Added new methods

--- PairProgrammingGame/BlackJackUI.cs
+++ PairProgrammingGame/BlackJackUI.cs
@@ -21,26 +21,72 @@ namespace PairProgrammingGame
         public void PlayGame()
         {
             // Bet
-            
+
+
             // Give Cards
+            Console.Clear();
             repo.Shuffle();
             repo.Deal();
             Card dealerCard1 = repo._dealerHand[0];
             Card dealerCard2 = repo._dealerHand[1];
-            Console.WriteLine($"The dealer is showing {dealerCard1.Number} of {dealerCard1.Suit}");
-            Console.WriteLine($"But his secret card is {dealerCard2.Number} of {dealerCard2.Suit}");
+            Card playerCard1 = repo._playerHand[0];
+            Card playerCard2 = repo._playerHand[1];
+            Console.WriteLine(" _______     _______\n" + "|       |   |       |\n" + $"| {dealerCard1.Letter}{(dealerCard1.Letter == "10" ? "" : " ")}    |   | ///// |\n" + $"|   {dealerCard1.Symbol}   |   | ///// |\n" + $"|    {(dealerCard1.Letter == "10" ? "" : " ")}{dealerCard1.Letter} |   | ///// |\n" + $"|_______|   |_______|\n");
+            Console.WriteLine($"The dealer is showing a {dealerCard1.Number} of {dealerCard1.Suit}\n");
+            Console.WriteLine();
+            // Create a display player card system
+
+            Console.WriteLine($"You were dealt a {playerCard1.Number} of {playerCard1.Suit} {playerCard1.Letter}{playerCard1.Symbol} & a {playerCard2.Number} of {playerCard2.Suit} {playerCard2.Letter}{playerCard2.Symbol}");
+
             // Player Actions
-            //while loop is repo._playerHand.total <= 21
-            //repo.PlayerHit();
+            PlayerOptions();
+            System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
+
+            
             // Dealer Actions
+            Console.WriteLine($"The dealer reveals his other card is a {dealerCard2.Number} of {dealerCard2.Suit}");
+            System.Threading.Thread.Sleep(TimeSpan.FromSeconds(2));
             repo.Dealer();
+            Console.WriteLine($"Dealer has {repo.DealerTotal()}\n");
+            System.Threading.Thread.Sleep(TimeSpan.FromSeconds(3));
+
+
             // Winner ? Dealer : Player
             repo.WinnerOutput();
+
+
             // Manage money
 
             // Loop
 
+            // This is for testing only, take out in full release.
+            Console.ReadLine();
         }
 
-}
+        public void PlayerOptions()
+        {
+            bool playerContinue = true;
+            while (playerContinue)
+            {
+                Console.WriteLine($"Your card total is: {repo.PlayerTotal()}");
+                if (repo.PlayerTotal() > 21) { Console.WriteLine("You busted\n"); playerContinue = false; }
+                else if (repo.PlayerTotal() == 21) { Console.WriteLine("Black Jack!\n"); playerContinue = false; }
+                else {
+                    Console.WriteLine("Would you like to [H]it or [S]tand?");
+                string input = Console.ReadLine();
+                    switch (input.ToLower())
+                    {
+                        case "hit":
+                        case "h":
+                            repo.PlayerHit();
+                            break;
+                        case "stand":
+                        case "s":
+                            playerContinue = false;
+                            break;
+                    } 
+                }
+            }
+        }
+    }
 }
--- PairProgrammingGame/Deck.cs
+++ PairProgrammingGame/Deck.cs
@@ -8,24 +8,7 @@ namespace PairProgrammingGame
 {
     public class Deck
     {
-
         public Dictionary<int, Card> _deck = new Dictionary<int, Card>();
-
-        //List<TKey> keyList = new List<>(_deck);
-
-        //private List<Deck> _deck = new List<Deck>();
-        // Dictionary, random number pull, remove, Casey said "utilize hash set" foreach random number
-        //_deck.remove(cardDrawn);
-
-        // Jacob thought process
-        // So in a randomized dictionary we still have to randomize the indexes and match that up with items/objects
-        // Then still manage to pull a card and move it into the player's hand and the dealers hand
-        // Thinking it'd be better to make a straight forward dictionary of 1-52 cards and use a random number generator to pull a random index
-        // So instead of randomizing the list and pullng out. We can pull a random card from a straight deck, .remove that card from the dictionary
-        // Use a variable to say give me a random number between 0 and deckSize where every time we pull a card we --deckSize
-        // That way the playerhand<> list will be a tally and we can display all card objects within that list. Same for dealer.
-        // We can even utilize the same deck for multiple hands by keeping it and emptying the player and dealer lists until like the deckSize variable
-        // is less than 7 or something. Cw shuffling.
     }
     public class Card
     {
@@ -40,22 +23,38 @@ namespace PairProgrammingGame
         public int Value { get; set; }
         public Suit Suit { get; set; }
         public int Index { get; set; }
-        public char Symbol
+        public string Symbol
         {
             get
             {
                 switch (Suit) {
                     case Suit.Spades:
-                        return '\u2663';
+                        return "\u2660";
                     case Suit.Hearts:
-                        return '\u2663';
+                        return "\u2665";
                     case Suit.Clubs:
-                        return '\u2663';
+                        return "\u2663";
                     case Suit.Diamonds:
-                        return '\u2663';
+                        return "\u2666";
                     default:
-                        return '\u2663';
+                        return "\u2663";
                 }
+            }
+        }
+        public string Letter
+        {
+            get
+            {
+                if (Number == "Ace")
+                    return "A";
+                else if (Number == "King")
+                    return "K";
+                else if (Number == "Queen")
+                    return "Q";
+                else if (Number == "Jack")
+                    return "J";
+                else
+                    return Convert.ToString(Value);                    
             }
         }
     }
@@ -66,6 +65,4 @@ namespace PairProgrammingGame
         Hearts,
         Diamonds
     }
-    //public enum suit
-    //{♠,♥,♣,♦}
 }
